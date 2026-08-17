0e71e79048622ed5836fb146420fc341caf4c71e
Daksh Agarwal <dakshagarwal889@gmail.com>
2026-08-18 00:18:14 +0530

    Create 210-Course-Schedule-2.cpp

---
+++ Graphs/210-Course-Schedule-2.cpp
@@ -0,0 +1,43 @@
+// LeetCode - 
+
+class Solution {
+    private:
+    bool dfs(int node,vector<vector<int>> &adj,vector<int> &vis,stack<int> &st,vector<int> &pathVis){
+        vis[node] = 1;
+        pathVis[node] = 1;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                if(dfs(it,adj,vis,st,pathVis) == true) return true;
+            }
+            else if(pathVis[it] == true) return true;
+        }
+        st.push(node);
+        pathVis[node] = 0;
+        return false;
+    }
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> vis(numCourses,0);
+        vector<int> pathVis(numCourses,0);
+        vector<vector<int>> adj(numCourses);
+        stack<int> st;
+        vector<int> ans;
+        for(auto p :prerequisites){
+            int second = p[0];
+            int first = p[1];
+            adj[first].push_back(second);
+        }
+        for(int i = 0;i<numCourses;i++){
+            if(!vis[i]){
+                if(dfs(i,adj,vis,st,pathVis)){
+                    return {};
+                }
+            }
+        }
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        return ans;
+    }
+};
