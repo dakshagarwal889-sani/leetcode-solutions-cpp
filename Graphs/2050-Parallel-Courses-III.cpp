087d92a5a37a12d8322c137ff80b0a8dafe29338
Daksh Agarwal <dakshagarwal889@gmail.com>
2026-08-18 22:20:35 +0530

    Implement minimumTime function for course scheduling

---
+++ Graphs/2050-Parallel-Courses-III.cpp
@@ -0,0 +1,36 @@
+
+
+class Solution {
+public:
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        vector<int> vis(n);
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n,0);
+        vector<int> maxTime(n,0);
+        maxTime = time;
+        queue<int> q;
+        for(auto r:relations){
+            int first = r[0]-1;
+            int second = r[1]-1;
+            adj[first].push_back(second);
+            indegree[second]++;
+        }
+        for(int i = 0;i<n;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        int ans;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans = max(ans, maxTime[node]);
+            for(auto it: adj[node]){
+                maxTime[it] = max(maxTime[it],maxTime[node] + time[it]);
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        return ans;
+    }
+};
